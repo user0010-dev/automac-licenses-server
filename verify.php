317abac413c95ae1444644bc264f89c823915c50
user0010-dev <xczcreations@gmail.com>
2025-08-25 02:17:25 +0200

    verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,11 @@
 <?php
-header('Content-Type: text/plain');
-
+// Verifica licenza semplice per raw.githubusercontent.com
 $license_key = $_GET['key'] ?? '';
 
-// Leggi il file licenses.json
-$data = file_get_contents('licenses.json');
-$licenses = json_decode($data, true);
+// URL del file licenses.json
+$json_url = 'https://raw.githubusercontent.com/user0010-dev/automac-licenses-server/main/licenses.json';
+$json_data = file_get_contents($json_url);
+$licenses = json_decode($json_data, true);
 
 if (isset($licenses['licenses'][$license_key])) {
     $license = $licenses['licenses'][$license_key];
