eee639b2ff18886352e9480d25091cda77b6c713
user0010-dev <xczcreations@gmail.com>
2025-08-25 02:41:29 +0200

    verify.php

---
+++ verify.php
@@ -0,0 +1,17 @@
+<?php
+$license_key = $_GET['key'] ?? '';
+$json_url = 'https://raw.githubusercontent.com/user0010-dev/automac-licenses-server/main/licenses.json';
+$json_data = file_get_contents($json_url);
+$licenses = json_decode($json_data, true);
+
+if (isset($licenses['licenses'][$license_key])) {
+    $license = $licenses['licenses'][$license_key];
+    if ($license['status'] === 'active') {
+        echo "VALID";
+    } else {
+        echo "INACTIVE";
+    }
+} else {
+    echo "NOT_FOUND";
+}
+?>
